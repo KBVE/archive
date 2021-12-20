3756d3471e4104cf9a9d596b1449c938b0cc1fdd
h0lybyte <admin@kbve.com>
2021-12-20 16:46:56 -0500

    Update cron.sh

--- sh/whmcs/cron.sh
+++ sh/whmcs/cron.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 #CronJob - based off of https://docs.whmcs.com/Crons
 #Example - */5 * * * * php -q /home/username/crons/cron.php
 #wget -q "http://blah.meh.com/my/path" -O /dev/null
 #wget -O http://www.kbve.com/whmcspath/crons/cron.php >/dev/null
-wget -O http://www.kbve.com/whmcspath/crons/cron.php >/dev/null
 #We can go ahead and add a nginx layer ontop to prevent access to the file from any IPs besides the one listed.
+
+wget -O http://www.kbve.com/whmcspath/crons/cron.php >/dev/null
