a58e51418e2a3a57f5783ce67085e0735979d5e3
h0lybyte <admin@kbve.com>
2022-03-17 17:25:56 -0400

    Update _install_-anbox-.sh

--- sh/anbox/_install_-anbox-.sh
+++ sh/anbox/_install_-anbox-.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Dont Forget to chmod +x
+# Download ```wget https://raw.githubusercontent.com/KBVE/archive/main/sh/anbox/_install_-anbox-.sh```
+# Permissions ```chmod +x _install_-anbox-.sh```
 sudo apt-get update
 sudo snap install --devmode --beta anbox -y
 sudo apt-get install android-tools-adb -y
