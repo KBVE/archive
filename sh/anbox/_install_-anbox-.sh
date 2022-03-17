b55c3cf47d496ebe70fa1e8ee64906901c1fba83
h0lybyte <admin@kbve.com>
2022-03-17 18:11:43 -0400

    Update _install_-anbox-.sh

--- sh/anbox/_install_-anbox-.sh
+++ sh/anbox/_install_-anbox-.sh
@@ -3,15 +3,15 @@
 # Permissions ```chmod +x _install_-anbox-.sh```
 echo "[|0|] Install Started! Please wait."
 echo "[1\] CREATE LOG"
-exec >anbox_install_log.txt 2>&1
+# exec >anbox_install_log.txt 2>&1
 echo "[/1] LOG CREATED"
 echo "[2\] UPDATING"
-sudo apt-get update
+sudo apt-get update >log_anbox_apt_update.txt 2>&1 |
 echo "[/2] UPDATED"
 echo "[3\] SNAP INSTALL"
-sudo snap install --devmode --beta anbox
+sudo snap install --devmode --beta anbox >log_anbox_snap_install.txt 2>&1 |
 echo "[/3] SNAP ANBOX COMPLETED"
 echo "[4\] Android Tools ADB being added"
-sudo apt-get install android-tools-adb -y
+sudo apt-get install android-tools-adb -y >log_anbox_adb_install.txt 2>&1 |
 echo "[/4] ADB Completed"
 echo "!END!"
