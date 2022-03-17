dd2a0365efd11fc9c8effba25040e8f70f1c2427
h0lybyte <admin@kbve.com>
2022-03-17 19:16:32 -0400

    Update _install_-anbox-.sh

--- sh/anbox/_install_-anbox-.sh
+++ sh/anbox/_install_-anbox-.sh
@@ -2,22 +2,49 @@
 # Download ```wget https://raw.githubusercontent.com/KBVE/archive/main/sh/anbox/_install_-anbox-.sh```
 # Permissions ```chmod +x _install_-anbox-.sh```
 
-LOGDIR="$(pwd)/anbox-logs"
-if [ ! -d "$LOGDIR" ]; then
-    mkdir "$LOGDIR"
+if [ ! "$(which sudo)" ]; then
+	SUDO=""
+else
+	SUDO=$(which sudo)
 fi
 
+
 echo "[~0]  INSTALL STARTED"
-echo "[1\]  CREATE  LOG"
-# exec >anbox_install_log.txt 2>&1
-echo "[/1]  LOG     CREATED"
+echo "[1\]  CREATE  DIR"
+
+KBVE="$(pwd)/kbve"
+if [ ! -d "$KBVE" ]; then
+    $SUDO mkdir "$KBVE"
+fi
+
+LOGDIR="$KBVE/logs"
+if [ ! -d "$LOGDIR" ]; then
+    $SUDO mkdir "$LOGDIR"
+fi
+
+
+echo "[/1]  DIR     CREATED"
+
 echo "[2\]  APT     UPDATING"
-sudo apt-get update 2>&1 | tee "$LOGDIR/log_anbox_apt_update.txt"
+$SUDO apt-get update 2>&1 | tee "$LOGDIR/log_anbox_apt_update.txt"
 echo "[/2]  APT     UPDATED"
+
 echo "[3\]  SNAP    INSTALL"
-sudo snap install --devmode --beta anbox 2>&1 | tee "$LOGDIR/log_anbox_snap_install.txt"
+$SUDO snap install --devmode --beta anbox 2>&1 | tee "$LOGDIR/log_anbox_snap_install.txt"
 echo "[/3]  SNAP    ANBOX       DONE"
+
 echo "[4\]  APT     INSTALL"
-sudo apt-get install android-tools-adb wget curl lzip tar unzip squashfs-tools -y 2>&1 | tee "$LOGDIR/log_anbox_apt_install.txt"
+$SUDO apt-get install android-tools-adb wget curl lzip tar unzip squashfs-tools -y 2>&1 | tee "$LOGDIR/log_anbox_apt_install.txt"
 echo "[/4]  APT     DONE"
+
+echo "[5\]  PLAY    INSTALL"
+$SUDO cd "$KBVE"
+$SUDO wget https://raw.githubusercontent.com/geeks-r-us/anbox-playstore-installer/master/install-playstore.sh
+$SUDO chmod +x install-playstore.sh
+$SUDO bash ./install-playstore.sh 2>&1 | tee "$LOGDIR/log_anbox_play_install.txt"
+echo "[/5]  PLAY    DONE"
+
+echo "[L] Launch"
+$SUDO anbox.appmgr
+
 echo "!END!"
